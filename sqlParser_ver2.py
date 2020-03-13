c7008e3e97740d79d1402eabddbd77bca53ce739
김상완 <sangwan_kim@tmax.co.kr>
2020-03-13 15:50:09 +0900

    whitespace처리 커밋

--- sqlParser_ver2.py
+++ sqlParser_ver2.py
@@ -3,69 +3,93 @@ from sqlparse.sql import Where, Comparison, Parenthesis, Identifier, IdentifierL
 
 
 class RecursiveTokenParser(object):
- def __init__(self, query):
-    self.query = query
-    self.names = []
-    self.elements = sqlparse.parse(self.query)
-    self.stmt = self.elements[0]
- def extractColumn(self):
-     query_type = self.stmt.get_type()
-     if self.stmt.get_type() == 'INSERT' :
-         print("insert query")
-         return self.insert_query()
-     else :
-        if self.stmt.get_type() == 'SELECT' :
-            print("select query")
+    def __init__(self, query):
+        self.query = query
+        self.names = []
+        self.elements = sqlparse.parse(self.query)
+        self.stmt = self.elements[0]
+        #token 안에 존재하는 whitespace (빈칸) 정리
+        for token in self.stmt.tokens:
+            if token.is_whitespace:
+                self.stmt.tokens.pop(self.stmt.tokens.index(token))
 
- def insert_query(self):
-    #elements = sqlparse.parse(self.query)
-    #print(elements[0].tokens)
-    for token in self.stmt.tokens:
-        if isinstance(token, Identifier):
-            self.identifier(token)
-        elif isinstance(token, IdentifierList):
-            print(token)
-            self.identifier(token)
-        elif isinstance(token, Parenthesis):
-            self.parenthesis(token)
-        elif isinstance(token, Where):
-            self.where(token)
 
-    return [str(name).upper() for name in self.names]
+    def extractColumn(self):
+         query_type = self.stmt.get_type()
+         if query_type == 'INSERT' :
+             print("This SQL is a INSERT DML")
+             return self.insert_query()
+         else:
+             query_type == 'SELECT'
+             print("This SQL is a SELECT DML")
+             return self.select_query()
 
- def where(self, token):
 
-    for subtoken in token.tokens:
-        if isinstance(subtoken, Comparison):
-            self.comparison(subtoken)
+    def insert_query(self):
+        #elements = sqlparse.parse(self.query)
+        #print(elements[0].tokens)
+        for token in self.stmt.tokens:
+            if isinstance(token, Identifier):
+                self.identifier(token)
+            elif isinstance(token, IdentifierList):
+                self.identifier(token)
+        return [str(name).upper() for name in self.names]
 
- def comparison(self, token):
-    for subtoken in token.tokens:
-        if isinstance(subtoken, Parenthesis):
-            self.parenthesis(subtoken)
 
- def parenthesis(self, token):
+    def select_query(self):
+        for token in self.stmt.tokens:
+            if isinstance(token, Identifier):
+                self.identifier(token)
+            elif isinstance(token, IdentifierList):
+                self.identifier(token)
+            elif isinstance(token, Parenthesis):
+                self.parenthesis(token)
+            elif isinstance(token, Where):
+                self.where(token)
+        return [str(name).upper() for name in self.names]
 
-    for subtoken in token.tokens:
-        if isinstance(subtoken, Identifier):
-            self.identifier(subtoken)
-        elif isinstance(subtoken, Parenthesis):
-            self.parenthesis(subtoken)
+    def where(self, token):
+        for subtoken in token.tokens:
+            if isinstance(subtoken, Comparison):
+                self.comparison(subtoken)
+            elif isinstance(subtoken, Parenthesis):
+                self.parenthesis(subtoken)
+            elif isinstance(subtoken, Where):
+                self.where(subtoken)
 
- def identifier(self, token):
-    self.names.append(token)
 
- def identifierList(self, token):
-     self.names.append(token)
+    def comparison(self, token):
+        for subtoken in token.tokens:
+            if isinstance(subtoken, Parenthesis):
+                self.parenthesis(subtoken)
+            elif isinstance(subtoken, Identifier):
+                self.identifier(subtoken)
 
- def get_query(self):  #
-    return self.query
+    def parenthesis(self, token):
+        for subtoken in token.tokens:
+            print(subtoken)
+            if isinstance(subtoken, Identifier):
+                self.identifier(subtoken)
+            elif isinstance(subtoken, Where) :
+                self.where(token)
+            elif isinstance(subtoken, Parenthesis):
+                self.parenthesis(subtoken)
 
-sqlInsert = "INSERT INTO SCHEMA1.TABLE1 (A, B, C, D, E, F, G) VALUES (1,2,3,4,5,6,7)"
-sqlSelect = "SELECT a FROM CITY WHERE a = (SELECT e FROM  Country)"
-insertTest = RecursiveTokenParser(sqlInsert)
-selectTest = RecursiveTokenParser(sqlSelect)
-print(insertTest.extractColumn())
-print(selectTest.extractColumn())
+
+    def identifier(self, token):
+        self.names.append(token)
+
+    def identifierList(self, token):
+        self.names.append(token)
+
+    def get_query(self):
+        return self.query
+
+sqlInsert = "INSERT INTO SCHEMA1.TABLE1 (A, B, C, D, E, F, G) VALUES (1,2,3,4,5,6,7);"
+sqlSelect = "SELECT c.A FROM CITY as c WHERE (SELECT B.A FROM BUG as b WHERE b.f = 3);"
+it = RecursiveTokenParser(sqlInsert)
+st = RecursiveTokenParser(sqlSelect)
+print(it.extractColumn())
+print(st.extractColumn())
 #print(t.get_query())
 #print(t.insert_query())
