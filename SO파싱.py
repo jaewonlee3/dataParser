18b82c07d4f9d0f77e7ecc08c5b98c09dcbe68ef
김상완 <sangwan_kim@tmax.co.kr>
2020-03-27 13:27:21 +0900

    Update SO파싱.py
    sqlpo parser merge 부분 작업

--- SO파싱.py
+++ SO파싱.py
@@ -80,11 +80,15 @@ def SetQueryParser(SetQuery, QueryList3, Metapath, doCall) :
 
 def QueryExtractor(QueryList4, DOFtree, AliasName, StatementType) :
     for statements in DOFtree.iter(StatementType) :
-        if (statements.get('alias') == AliasName) :
+        if (statements.get('alias') == AliasName):
             Query = statements.find(DofStatement).text
-            QueryList4.append(Query)
-            # 쿼리문까지 도달했으므로 하나의 리스트 완성. 원래의 QueryLists에 append 해준다.
-            QueryLists.append(QueryList4)
+            processedQuery = Query.replace('\n', ' ')
+            columnList = RecursiveTokenParser(processedQuery).extractColumn()
+            print(columnList)
+            QueryList4.append(processedQuery)
+            for column in columnList:
+                # 쿼리문까지 도달했으므로 하나의 리스트 완성. 원래의 QueryLists에 append 해준다.
+                QueryLists, append(QueryList4 + column)
     return 0
 
 def PoParser(SGpath) :
