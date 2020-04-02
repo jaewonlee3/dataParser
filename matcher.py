2db92afd5015a3963b1be9631d5f51bb4e26e3f8
Jaewon Lee <jaewon_lee2@tmax.co.kr>
2020-04-02 10:16:03 +0900

    dd

--- matcher.py
+++ matcher.py
@@ -116,13 +116,13 @@ def inputValueFormal(totalDic, jsDic, xmlDic):
 
 
 
-jsFileList = jsParserNew.search("C:/Users/이재원/Documents/fsCode/FI_TOP_1Q-feature")
-
-jsList = jsParserNew.readJsFile(jsFileList)
-
-xmlFileList = xmlParser.search("C:/Users/이재원/Documents/fsCode/FI_TOP_1Q-feature")
-xmlList = xmlParser.readTlfFile(xmlFileList)
-
-
-kk = matchXmlAndJs(xmlList, jsList)
-printTotal(kk)
+# jsFileList = jsParserNew.search("C:/Users/이재원/Documents/fsCode/FI_TOP_1Q-feature")
+#
+# jsList = jsParserNew.readJsFile(jsFileList)
+#
+# xmlFileList = xmlParser.search("C:/Users/이재원/Documents/fsCode/FI_TOP_1Q-feature")
+# xmlList = xmlParser.readTlfFile(xmlFileList)
+#
+#
+# kk = matchXmlAndJs(xmlList, jsList)
+# printTotal(kk)
